739facc7ef76516992ffbbefdc7269e0440ad0ab
Mohamed <ahnbi32@gmail.com>
2024-05-08 11:22:28 +0100

    adding user entity

---
+++ backend/pagination/src/main/java/com/mohamed/pagination/entity/User.java
@@ -0,0 +1,26 @@
+package com.mohamed.pagination.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String status;
+    private String address;
+    private String phone;
+    private String imageURl;
+
+}
