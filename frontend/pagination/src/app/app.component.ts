b87201e9554f7e4ed0a83aa28bfb306c045f7635
Mohamed <ahnbi32@gmail.com>
2024-05-08 22:49:33 +0100

    update code

--- frontend/pagination/src/app/app.component.ts
+++ frontend/pagination/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './model/user';
+import { HttpErrorResponse } from '@angular/common/http';
+import { UserService } from './service/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pagination';
+export class AppComponent implements OnInit{
+ // usersState$?: Observable<{appState: String, appData?: User, error?:HttpErrorResponse}>;
+
+ constructor(private userService: UserService){}
+
+  ngOnInit(): void {
+    this.initializeUsers();
+  }
+
+  initializeUsers(){
+    this.userService.getUsers().subscribe({
+      next:(responce :any)=>{
+        console.log("Responce" , responce)
+      },
+      error:(error:any)=>{
+        console.log("Error " + error)
+      }
+    })
+  }
+
 }
---
+++ frontend/pagination/src/app/service/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private readonly API_GET_USERS :string = "http://localhost:8086/pagination/get/users"
+  constructor(private http:HttpClient) { }
+
+  getUsers(name:string = '',page:number = 0,size:number = 10):Observable<User> {
+    return this.http.get<User>(`${this.API_GET_USERS}`,{
+      params:{
+        name:name,
+        page:page,
+        size:size,
+      }
+    })
+  }
+}
