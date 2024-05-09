c7fbde4f0d560cb4ac5abff323476eaac8554e80
Mohamed <ahnbi32@gmail.com>
2024-05-09 18:47:55 +0100

    update code

--- frontend/pagination/src/app/app.component.ts
+++ frontend/pagination/src/app/app.component.ts
@@ -13,6 +13,8 @@ export class AppComponent implements OnInit{
  // usersState$?: Observable<{appState: String, appData?: User, error?:HttpErrorResponse}>;
  usersList:User[] = [];
  numberOfpages: number[] = [];
+ private currentPageNumber:number = 0;
+
  constructor(private userService: UserService){}
 
   ngOnInit(): void {
@@ -26,6 +28,7 @@ export class AppComponent implements OnInit{
         this.usersList = responce.userList
         let pages = responce.pageNumbers
         this.numberOfpages = Array.from({length: pages}, (_, index) => index);
+        this.currentPageNumber = responce.currentPage
       },
       error:(error:any)=>{
         console.log("Error " + error)
@@ -33,18 +36,40 @@ export class AppComponent implements OnInit{
     })
   }
 
-  goToPage(name:string | null, pageNumber?:number){
+  goToPage(name:string, pageNumber?:number){
     this.userService.getUsers(name!, pageNumber).subscribe({
       next:(responce :any)=>{
         console.log("Responce" , responce)
         this.usersList = responce.userList
         let pages = responce.pageNumbers
         this.numberOfpages = Array.from({length: pages}, (_, index) => index);
+        this.currentPageNumber = responce.currentPage
       },
       error:(error:any)=>{
         console.log("Error " + error)
       }
     })
   }
+
+  goToNextOrPrevPage(direction: string, name: string) {
+    let currentPageIndex = this.currentPageNumber;
+    if (direction === 'prev') {
+        if (currentPageIndex > 0) {
+            currentPageIndex--;
+        } else {
+            return;
+        }
+    } else if (direction === 'next') {
+        if (currentPageIndex < this.numberOfpages.length - 1) {
+            currentPageIndex++;
+        } else {
+            return;
+        }
+    }
+
+    const pageNumber = this.numberOfpages[currentPageIndex];
+    this.goToPage(name, pageNumber);
+}
+
 
 }
